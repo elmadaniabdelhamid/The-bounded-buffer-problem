e8e66d676915d216b61124eb248c551b06c77597
Abdelhamid El Madani <149427507+elmadaniabdelhamid@users.noreply.github.com>
2024-06-20 13:09:50 +0100

    Update MainBlockingQueue.java

--- MainBlockingQueue.java
+++ MainBlockingQueue.java
@@ -1,4 +1,4 @@
-package khemal_project;
+package The-bounded-buffer-problem;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
